50e07a45d7d3b82b75470313aa19ee9b0eb3b811
saikrishna76845 <saidusa221@gmail.com>
2025-07-15 14:11:16 +0530

    routing paths removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 const routes: Routes = [
   {path:"", component:DashboardComponent}
